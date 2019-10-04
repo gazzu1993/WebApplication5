ae3fda3723c49aa74e1b7248e460bbb86a433e75
gajendrajangid.it <gajendrajangid.it@gmail.com>
2019-10-04 07:45:25 +0530

    Add project files.

---
+++ WebApplication5/Controllers/CurdController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Data;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class CurdController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DBClass obj = new DBClass("Usp_SetEmployee", CommandType.StoredProcedure))
+                {
+                    obj.AddParameters("ID", employee.ID);
+                    obj.AddParameters("Name", employee.Name);
+                    obj.AddParameters("Department", employee.Department);
+                    obj.AddParameters("Gender", employee.Gender);
+                    obj.AddParameters("City", employee.City);
+                    obj.ExecuteNonQuery();
+                }
+                return RedirectToAction("List");
+            }
+            return View(employee);
+        }
+        public IActionResult List()
+        {
+            using (DBClass obj = new DBClass("Usp_GetEmployee", CommandType.StoredProcedure))
+            {
+                DataTable dt = obj.ReturnDataTable();
+                List<Employee> EmployeeList = new List<Employee>();
+                EmployeeList = (from DataRow dr in dt.Rows
+                                select new Employee()
+                                {
+                                    ID = Convert.ToInt32(dr["ID"]),
+                                    Name = dr["Name"].ToString(),
+                                    Department = dr["Department"].ToString(),
+                                    Gender = dr["Gender"].ToString(),
+                                    City = dr["City"].ToString()
+                                }).ToList();
+                return View(EmployeeList);
+            }
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            using (DBClass obj = new DBClass("Usp_GetEmployee", CommandType.StoredProcedure))
+            {
+                obj.AddParameters("ID", id.ToString());
+                DataTable dt = obj.ReturnDataTable();
+                Employee EmployeeList = new Employee();
+                EmployeeList = (from DataRow dr in dt.Rows
+                                select new Employee()
+                                {
+                                    ID = Convert.ToInt32(dr["ID"]),
+                                    Name = dr["Name"].ToString(),
+                                    Department = dr["Department"].ToString(),
+                                    Gender = dr["Gender"].ToString(),
+                                    City = dr["City"].ToString()
+                                }).FirstOrDefault();
+                if (EmployeeList == null)
+                {
+                    return NotFound();
+                }
+                return View(EmployeeList);
+            }
+
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind]Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                using (DBClass obj = new DBClass("Usp_SetEmployee", CommandType.StoredProcedure))
+                {
+                    obj.AddParameters("ID", employee.ID);
+                    obj.AddParameters("Name", employee.Name);
+                    obj.AddParameters("Department", employee.Department);
+                    obj.AddParameters("Gender", employee.Gender);
+                    obj.AddParameters("City", employee.City);
+                    obj.ExecuteNonQuery();
+                }
+                return RedirectToAction("List");
+            }
+            return View(employee);
+        }
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            using (DBClass obj = new DBClass("Usp_GetEmployee", CommandType.StoredProcedure))
+            {
+                obj.AddParameters("ID", id.ToString());
+                DataTable dt = obj.ReturnDataTable();
+                Employee EmployeeList = new Employee();
+                EmployeeList = (from DataRow dr in dt.Rows
+                                select new Employee()
+                                {
+                                    ID = Convert.ToInt32(dr["ID"]),
+                                    Name = dr["Name"].ToString(),
+                                    Department = dr["Department"].ToString(),
+                                    Gender = dr["Gender"].ToString(),
+                                    City = dr["City"].ToString()
+                                }).FirstOrDefault();
+                if (EmployeeList == null)
+                {
+                    return NotFound();
+                }
+                return View(EmployeeList);
+            }
+        }
+
+    }
+}
---
+++ WebApplication5/Models/DBClass.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+ 
+
+
+public class DBClass : IDisposable
+{
+    SqlConnection con;
+    SqlCommand cmd;
+    string strConnection = "";
+    public DBClass(string spname, CommandType cmdtype)
+    {
+        strConnection = "Server=tcp:gazzusql.database.windows.net,1433;Initial Catalog=Gazzu_Db1;Persist Security Info=False;User ID=gajendraq3;Password=gajendrajangid@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        con = new SqlConnection(strConnection);
+        cmd = new SqlCommand(spname);
+        cmd.CommandType = cmdtype;
+        cmd.Connection = con;
+
+        if (con.State == ConnectionState.Closed)
+        con.Open();
+    }
+    public DBClass(string spname, CommandType cmdtype, string ConnStr)
+    {
+        strConnection = ConnStr;
+        con = new SqlConnection(strConnection);
+
+        cmd = new SqlCommand(spname);
+        cmd.CommandType = cmdtype;
+        cmd.Connection = con;
+
+        if (con.State == ConnectionState.Closed)
+            con.Open();
+    }
+    public void AddParameters(string pname, string pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.NVarChar).Value = pvalue;
+    }
+    public void AddParameters(string pname, DataTable pvalue)
+    {
+        SqlParameter PrmTbl = cmd.Parameters.AddWithValue(pname, pvalue);
+        PrmTbl.SqlDbType = SqlDbType.Structured;
+        PrmTbl.Direction = ParameterDirection.Input;
+    }
+    public void AddParameters(string pname, bool pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.Bit).Value = pvalue;
+    }
+    public void AddParameters(string pname, System.DBNull pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.NVarChar).Value = pvalue;
+    }
+    public void AddParameters(string pname, Int64 pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.BigInt).Value = pvalue;
+    }
+    public void AddParameters(string pname, int pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.Int).Value = pvalue;
+    }
+    public void AddParameters(string pname, float pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.Float).Value = pvalue;
+    }
+    public void AddParameters(string pname, DateTime pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.DateTime).Value = pvalue;
+    }
+    public void AddParameters(string pname, Decimal pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.Decimal).Value = pvalue;
+    }
+    public void AddParameters(string pname, Double pvalue)
+    {
+        cmd.Parameters.Add(pname, SqlDbType.Decimal).Value = pvalue;
+    }
+    //public void AddParameters(string pname, DataTable pvalue)
+    //{
+    //    cmd.Parameters.AddWithValue(pname, pvalue);
+    //}
+
+
+    public DataTable ReturnDataTable()
+    {
+        using (DataTable dt = new DataTable())
+        {
+            dt.Load(cmd.ExecuteReader());
+            return dt;
+        }
+    }
+    public int ExecuteNonQueryint()
+    {
+        return cmd.ExecuteNonQuery();
+    }
+    public DataSet ReturnDataSet()
+    {
+        using (DataSet ds = new DataSet())
+        {
+            using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+            {
+                adapter.Fill(ds);
+            }
+            return ds;
+        }
+    }
+
+    public void TimeOut(int time)
+    {
+        cmd.CommandTimeout = time;
+    }
+
+    public string ReturnString()
+    {
+        return Convert.ToString(cmd.ExecuteScalar());
+    }
+
+    public void ExecuteNonQuery()
+    {
+        cmd.ExecuteNonQuery();
+    }
+
+    public int ExecuteNonQueryWithReturn()
+    {
+        return Convert.ToInt16(cmd.ExecuteNonQuery());
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            // get rid of managed resources
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+                con.Dispose();
+            }
+            cmd.Dispose();
+        }
+    }
+}
